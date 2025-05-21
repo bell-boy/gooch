baf23bc6d2654f534bc9b979f3f0faa7cfe20e3a
SriniV4 <srini.vadhi@gmail.com>
2025-05-20 19:08:58 -0500

    MNISTgit add .

--- demos/mnist/train.cc
+++ demos/mnist/train.cc
@@ -5,35 +5,35 @@
 
 #include <iostream>
 
-/* int main(int argc, char** argv) { */
-/*   if (argc != 2) { */
-/*     std::cerr << "Usage: " << argv[0] << " <path_to_mnist_csv>" << std::endl; */
-/*     return 1; */
-/*   } */
-/*   std::string path = argv[1]; */
-/*   auto [labels, images] = GetMnist(path); */
-/*   gooch::Tensor x = gooch::FromVector(images); */
-/*   GatedLinearUnitMLP mlp(784, 100, 10); */
-/*   gooch::SGD sgd(mlp.params(), 1e-3f); */
-/*   int BATCH_SIZE = 10; */
-/*   // int count = 0; */
-/*   for (int i = 0; i + BATCH_SIZE <  (int) labels.size(); i+=BATCH_SIZE) { */
-/*     std::vector<size_t> y_true; */
-/*     for (int j = i; j < BATCH_SIZE + i; j++) { */
-/*       y_true.push_back(labels[j]); */
-/*     } */
-/*     // gooch::Tensor x_ = x(gooch::Slice(i,i+BATCH_SIZE-1)); */
-/*     gooch::Tensor x_ = x(gooch::Slice(0, BATCH_SIZE-1)); */
-/*     gooch::Tensor y_pred = mlp.forward(x_); */
+ int main(int argc, char** argv) { 
+   if (argc != 2) { 
+     std::cerr << "Usage: " << argv[0] << " <path_to_mnist_csv>" << std::endl; 
+     return 1; 
+   } 
+   std::string path = argv[1]; 
+   auto [labels, images] = GetMnist(path); 
+   gooch::Tensor x = gooch::FromVector(images); 
+   GatedLinearUnitMLP mlp(784, 100, 10); 
+   gooch::SGD sgd(mlp.params(), 1e-3f); 
+   int BATCH_SIZE = 10; 
+   // int count = 0; 
+   for (int i = 0; i + BATCH_SIZE <  (int) labels.size(); i+=BATCH_SIZE) { 
+     std::vector<size_t> y_true; 
+     for (int j = i; j < i+BATCH_SIZE; j++) { 
+       y_true.push_back(labels[j]); 
+     } 
+     gooch::Tensor x_ = x(gooch::Slice(i,i+BATCH_SIZE-1)); 
+    //  gooch::Tensor x_ = x(gooch::Slice(0, BATCH_SIZE-1)); 
+     gooch::Tensor y_pred = mlp.forward(x_); 
 
-/*     gooch::Tensor loss = gooch::crossEntropyLoss(y_pred, y_true); */
+     gooch::Tensor loss = gooch::crossEntropyLoss(y_pred, y_true); 
 
-/*     std::cout << loss << std::endl; */
+     std::cout << loss << std::endl; 
 
-/*     mlp.ZeroGrad(); */
-/*     loss.Backward(); */ 
-/*     sgd.step(); */
+     mlp.ZeroGrad(); 
+     loss.Backward();  
+     sgd.step(); 
 
-/*   } */
-/*   return 0; */
-/* } */
+   } 
+   return 0; 
+ }
